a2ebcc5c1dc10eef15b981071772a37f7358be5b
sszledak <sszledak@users.noreply.github.com>
2017-08-20 20:27:21 +0200

    Add styling

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
 $(document).ready(function () {
     var calculateBtn = $("#calculateBtn");
-    var msg = $("#msgId");
+
+    var alertError = "#alertError";
+    var alertBMIInfo = "#alertBMIInfo"
+    var alertResult = "#alertResult";
 
     calculateBtn.click(function () {
+
+        hideAllAlerts();
+
         var weight = parseFloat($("#weightId").val());
         var height = parseFloat($("#heightId").val());
 
-        var result = $("#resultId");
-
-        if(validateInputs(weight, height)) {
-            result.val(calculateBMI(weight, height).toFixed(2));
-            result.show();
-        } 
+        if (validateInputs(weight, height)) {
+            var result = calculateBMI(weight, height).toFixed(2);
+            showInormations(result);
+        }
     })
 
     function validateInputs(weight, height) {
-        console.log(weight < 0 || height < 0 || weight === 0 || height === 0);
-        if(weight < 0 || height < 0 || weight === 0 || height === 0) {
-            msg.val("Any value is less than 0 or is equals 0 !!!");
-            msg.show();
-            result.hide();
+
+        var errorMsg = "";
+
+        if (isNaN(weight)) {
+            errorMsg += "<p>Weight is undefined</p>";
+        }
+
+        if (isNaN(height)) {
+            errorMsg += "<p>Height is undefined</p>";
+        }
+
+        if (weight < 0) {
+            errorMsg += "<p>Weight is less than 0</p>";
+        }
+
+        if (height < 0) {
+            errorMsg += "<p>Height is less than 0</p>";
+        }
+
+        if (weight === 0) {
+            errorMsg += "<p>Weight is equals 0</p>";
+        }
+
+        if (height === 0) {
+            errorMsg += "<p>Height is equals 0 </p>";
+        }
+
+        if (errorMsg === "") {
+            hideAlert("#alertError");
+            return true;
+        } else {
+            createAlert("#alertError", "alert-danger", errorMsg);
             return false;
         }
-        msg.hide();
-        return true;
     }
 
-    function calculateBMI(weight, height) {   
-        var checked = $("input:checked" ).val(); 
-        console.log(checked);
+    function calculateBMI(weight, height) {
+        var checked = $("input:checked").val();
         if (checked === "m") {
             return calcMeters(weight, height);
         } else {
@@ -41,6 +69,44 @@ $(document).ready(function () {
     }
 
     function calcCentimeters(weight, height) {
-        return calcMeters(weight, height)*10000;
+        return calcMeters(weight, height) * 10000;
+    }
+
+    function showInormations(result) {
+        showResult(result);
+        showBMIInfo(result)
+    }
+
+    function showResult(result) {
+        var msg = "Your result is <strong>" + result + "</strong>";
+        createAlert(alertResult, "alert-info", msg);
+    }
+
+    function showBMIInfo(result) {
+        if (result < 18.5) {
+            createAlert(alertBMIInfo, "alert-danger", "<strong>Danger!</strong> You are underweight");
+        } else if (result > 18.5 && result < 24.99) {
+            createAlert(alertBMIInfo, "alert-success", "<strong>Success!</strong> You have normal or healthy weight");
+        } else if (result > 25 && result < 29.99) {
+            createAlert(alertBMIInfo, "alert-warning", "<strong>Warning!</strong> You are overweight");
+        } else if (result > 30) {
+            createAlert(alertBMIInfo, "alert-danger", "<strong>Danger!</strong> You are obese");
+        }
+
+    }
+
+    function createAlert(alertId, alertType, msg) {
+        $(alertId).html("<div class='alert " + alertType + "'>" + msg + "</div>");
+        $(alertId).show();
+    }
+
+    function hideAlert(alertId) {
+        $(alertId).hide();
+    }
+
+    function hideAllAlerts() {
+        hideAlert(alertResult);
+        hideAlert(alertError);
+        hideAlert(alertBMIInfo);
     }
 });
